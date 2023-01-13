128dc9f8e355e5d4b25e417e1c655b5fa606196f
milliorn <scottmilliorn@gmail.com>
2023-01-12 22:23:04 -0800

    Menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,8 @@
-const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn__burger");
+const menuBtn = document.querySelector(".menu-btn");
+const menuNav = document.querySelector(".menu-nav");
+const nav = document.querySelector(".nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
 
 let showMenu = false;
 
@@ -8,9 +11,15 @@ menuBtn.addEventListener("click", toggleMenu);
 function toggleMenu() {
   if (!showMenu) {
     hamburger.classList.add("open");
+    menuNav.classList.add("open");
+    nav.classList.add("open");
+    navItems.forEach((item) => item.classList.add("open"));
     showMenu = true;
   } else {
     hamburger.classList.remove("open");
+    menuNav.classList.remove("open");
+    nav.classList.remove("open");
+    navItems.forEach((item) => item.classList.remove("open"));
     showMenu = false;
   }
 }
