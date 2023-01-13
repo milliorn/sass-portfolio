a9ce0a0a91c0e99abe30378fb4c3cd0f412fcde9
milliorn <scottmilliorn@gmail.com>
2023-01-13 15:30:20 -0800

    refactor

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,25 +1,27 @@
-const hamburger = document.querySelector(".menu-btn__burger");
-const menuBtn = document.querySelector(".menu-btn");
-const menuNav = document.querySelector(".menu-nav");
-const nav = document.querySelector(".nav");
-const navItems = document.querySelectorAll(".menu-nav__item");
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
 
 let showMenu = false;
 
-menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-  if (!showMenu) {
-    hamburger.classList.add("open");
-    menuNav.classList.add("open");
-    nav.classList.add("open");
-    navItems.forEach((item) => item.classList.add("open"));
+  if(!showMenu) {
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItems.forEach(item => item.classList.add('open'));
+
     showMenu = true;
   } else {
-    hamburger.classList.remove("open");
-    menuNav.classList.remove("open");
-    nav.classList.remove("open");
-    navItems.forEach((item) => item.classList.remove("open"));
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItems.forEach(item => item.classList.remove('open'));
+
     showMenu = false;
   }
 }
